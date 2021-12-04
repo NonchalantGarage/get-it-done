bd4ee1d462c00de8a2df1344204cceb2608982ff
Curtis <hong.curtis@gmail.com>
2021-12-03 22:07:31 -0800

    set up HTML container, added function to display repos and searchTerm

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,61 @@
-var getUserRepos = function(){
-    fetch('https://api.github.com/users/octocat/repos');
+var userFormEL = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoSearchTermEl = document.querySelector("#repo-search-term");
+var reposContainerEl = document.querySelector("#repos-container");
+
+
+var getUserRepos = function(user){
+    // format the github api url
+    var apiUrl= "https:api.github.com/users/" + user + "/repos";
+
+    // Make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            displayRepos(data, user);
+        });
+    });
 };
-getUserRepos();
+
+
+
+var displayRepos = function (repos,searchTerm){
+    console.log(repos); 
+    console.log(searchTerm);
+    reposContainerEl.textContent = "";
+    repoSearchTermEl.textContent = searchTerm;
+};
+
+
+
+
+
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username){
+    getUserRepos(username);
+    nameInputEl.value = "";
+}   else {
+    alert("Please enter Github username");
+} 
+    console.log(event);
+};
+
+// This function will accept both the array of repository data and the term we searched for as parameters.
+
+
+
+// var response = fetch("https://api.github.com/users/octocat/repos").then(function(response){
+//     response.json().then(function(data){
+//         console.log(data);
+//     });
+// });
+
+
+userFormEL.addEventListener("submit", formSubmitHandler);   
+
+
 
